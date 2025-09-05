ea630e4aeea46d285cc4081ffc939f5763e5f93d
Muskan Dharewa <muskandharewa2007@gmail.com>
2025-09-05 19:37:53 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
-
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
+  base: "/NxtWaveHackathon/", // add this line
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
@@ -20,3 +10,4 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
+
